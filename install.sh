36df51cbe9e738b9e82fd35f1253baccd6c30a18
David Fan <fand.kanade@gmail.com>
2021-07-10 16:43:14 -0400

    Added kobuki, ecl, yocs packages

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 pip install empy pyem rospkg rospy
 
-sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev
+sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev
+
+#sudo apt install -y ros-noetic-ecl-streams
+sudo apt install -y ros-noetic-sophus
---
+++ setup-src.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -r misc/yujin_ocs/yocs_controllers src/dependencies
