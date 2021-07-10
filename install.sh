c1f6b68bbccd101187efcada024ccccc58ddc6c5
David Fan <fand.kanade@gmail.com>
2021-07-10 17:06:14 -0400

    Added gazebo simulator, functional with keyboard teleop

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-pip install empy pyem rospkg rospy
+#!/bin/bash
+
+# make sure conda environment for ros is active!
+# used with python3.8
+pip install numpy catkin_pkg empy pyem rospkg rospy defusedxml
 
 sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev
 
--- setup-src.sh
+++ setup-src.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cp -r misc/yujin_ocs/yocs_controllers src/dependencies
+mkdir -p src/dependencies/yocs
+cp -r misc/yujin_ocs/yocs_controllers \
+      misc/yujin_ocs/yocs_cmd_vel_mux \
+      src/dependencies/yocs
+
+cp -r misc/kobuki_desktop/kobuki_gazebo_plugins \
+      src/dependencies/kobuki
