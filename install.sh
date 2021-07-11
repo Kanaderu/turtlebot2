686741cfdc4c1000b544ce665422e90983ff33e5
David Fan <fand.kanade@gmail.com>
2021-07-10 22:55:43 -0400

    Added gstreamer gscam package

--- install.sh
+++ install.sh
@@ -4,8 +4,17 @@
 # used with python3.8 (pyqt5 used for kobuki_desktop package)
 pip install numpy catkin_pkg empy pyem rospkg rospy defusedxml pyqt5
 
-# install driver libraries
-sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev libfreenect-dev libsensors4-dev
+# kobuki
+sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev
 
-# install ros sophus
+# kinect
+sudo apt install -y libfreenect-dev
+
+# linux_peripheral_interfaces
+sudo apt install -y libsensors4-dev
+
+# ros sophus
 sudo apt install -y ros-noetic-sophus
+
+# gscam gstreamer (https://gstreamer.freedesktop.org/documentation/installing/on-linux.html)
+sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
