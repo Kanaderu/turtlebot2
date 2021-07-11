95097fe8eda056cc26f9c66c71e0dd11c6f033e8
David Fan <fand.kanade@gmail.com>
2021-07-10 20:38:10 -0400

    Updated kobuki_desktop requirements

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # make sure conda environment for ros is active!
-# used with python3.8
-pip install numpy catkin_pkg empy pyem rospkg rospy defusedxml
+# used with python3.8 (pyqt5 used for kobuki_desktop package)
+pip install numpy catkin_pkg empy pyem rospkg rospy defusedxml pyqt5
 
 sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev
 
