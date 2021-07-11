1cc14de5d673f20d7d1d48c6513e235c96ec08fa
David Fan <fand.kanade@gmail.com>
2021-07-10 22:25:18 -0400

    added kobuki initializer script

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 # used with python3.8 (pyqt5 used for kobuki_desktop package)
 pip install numpy catkin_pkg empy pyem rospkg rospy defusedxml pyqt5
 
+# install driver libraries
 sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev libfreenect-dev
 
-#sudo apt install -y ros-noetic-ecl-streams
+# install ros sophus
 sudo apt install -y ros-noetic-sophus
---
+++ setup_kobuki.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# setup kobuki base driver udev rules
+rosrun kobuki_ftdi create_udev_rules
