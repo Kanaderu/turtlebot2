13f7b55656d0c7df8cc47c7d61b76721c02b2516
David Fan <fand.kanade@gmail.com>
2021-07-10 22:03:14 -0400

    Added openni and freenect_stack for kinect

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # used with python3.8 (pyqt5 used for kobuki_desktop package)
 pip install numpy catkin_pkg empy pyem rospkg rospy defusedxml pyqt5
 
-sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev
+sudo apt install -y libusb-dev libbluetooth-dev libboost-all-dev libcwiid-dev libspnav-dev libftdi-dev libfreenect-dev
 
 #sudo apt install -y ros-noetic-ecl-streams
 sudo apt install -y ros-noetic-sophus
